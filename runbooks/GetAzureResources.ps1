5fd32273776d72ac095eefd84d02dac987534308
Andrew Kirkland <andrew@kirkland.nz>
2019-03-12 14:28:27 +1300

    Add Resource Group Name as a parameter.

--- runbooks/GetAzureResources.ps1
+++ runbooks/GetAzureResources.ps1
@@ -1,7 +1,13 @@
+#Parameters
+Param(
+	[string]$ResourceGroupName
+)
+
+# Authenticate using a Run As connection with Azure
 $connection = Get-AutomationConnection -Name AzureRunAsConnection
 Connect-AzureRmAccount -ServicePrincipal -Tenant $connection.TenantID `
 -ApplicationId $connection.ApplicationID -CertificateThumbprint $connection.CertificateThumbprint
 
 $AzureContext = Select-AzureRmSubscription -SubscriptionId $connection.SubscriptionID
 
-Get-AzureRmResource -ResourceGroupName "Dev-RG" -AzureRmContext $AzureContext
+Get-AzureRmResource -ResourceGroupName $ResourceGroupName -AzureRmContext $AzureContext
