26296d58d55b849bf38dbfb299e9b8e25172c4aa
Andrew Kirkland <andrew@kirkland.nz>
2019-03-18 15:43:33 +1300

    Add runbook to create Azure Web App and update README with more of a description of the project.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # odata-azure
-A project for creating a complete odata solution on Azure.
+A project for creating a complete OData solution on Azure.  This will expose APIs via the APIM where the underlying
+database is the sample AdventureWorksLT database.
+
+The PowerShell scripts in the runbooks folder create all the infrastructure resources that are needed
+for the OData API example.
+
+An OData API is provided using the simplest ASP.Net Azure App Service Web App.
---
+++ runbooks/CreateAzureWebApp.ps1
@@ -0,0 +1,24 @@
+# An Azure Automation Runbook to create an Azure App Service Plan and Azure Web App.
+
+#==============================================================================
+# Parameters
+Param(
+	[Parameter(Mandatory=$true)]
+	[string]$ResourceGroupName = "Dev-RG",
+	[Parameter(Mandatory=$true)]
+	[string]$AppServicePlanName = "OData-App-Svc-Plan"
+)
+
+#==============================================================================
+# Variables
+# Corresponds to the pricing tier to use.  Use the most basic for development.
+$Tier = "Free"
+
+# The Location to create the resources
+$Location = "australiasoutheast"
+
+#App Service Plan Workers
+$NumberOfWorkers = 1
+$WorkerSize = "Small"
+
+$appServicePlan = New-AzAppServicePlan -ResourceGroupName $ResourceGroupName -Name $AppServicePlanName -Tier $Tier -NumberOfWorkers $NumberOfWorkers
