081093975ed85eb1f03a1c6c1f0cf346af2efce4
Andrew Kirkland <andrew@kirkland.nz>
2019-03-19 15:52:04 +1300

    Add missing parameters in call to create web app.

--- runbooks/New-ODataDemo.ps1
+++ runbooks/New-ODataDemo.ps1
@@ -1,6 +1,16 @@
 #==============================================================================
 # The top level script for creating a complete OData API environment
 
+
+#==============================================================================
+# Parameters
+Param(
+	[Parameter(Mandatory=$true)]
+	[string]$ResourceGroupName = "Dev-RG",
+	[Parameter(Mandatory=$true)]
+	[string]$AppServicePlanName = "OData-App-Svc-Plan"
+)
+
 #==============================================================================
 # Authenticate using a Run As connection with Azure.  The Automation Account will exist within
 # a given Azure subscription and the AzureRunAsConnection is created when the Automation Account
@@ -16,4 +26,4 @@ $AzureContext = Set-AzContext -SubscriptionId $connection.SubscriptionID
 #==============================================================================
 # Create the Azure Web App that will provide the backend API onto the AdventureWorksLT database.
 
-.\New-AzureWebApp.ps1
+.\New-AzureWebApp.ps1 -ResourceGroupName $ResourceGroupName -AppServicePlanName $AppServicePlanName
